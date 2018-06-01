62143867e1253d59f9cddf1afaf108c8a49615ae
Admin <admin@admin.ad>
2018-06-01 13:10:03 +0300

    2ndC - Alpha release v.2

--- js/main.js
+++ js/main.js
@@ -4,8 +4,24 @@ $(document).ready(function () {
         const $mainSlider = $('#sliderDishes');
         const options = {
             infinite: true,
-            slidesToShow: 3,
-            slidesToScroll: 3
+            slidesToShow: 4,
+            slidesToScroll: 4,
+            responsive: [
+                {
+                    breakpoint: 1024,
+                    settings: {
+                        slidesToShow: 3,
+                        slidesToScroll: 3
+                    }
+                },
+                {
+                    breakpoint: 600,
+                    settings: {
+                        slidesToShow: 1,
+                        slidesToScroll: 1
+                    }
+                }
+            ]
         };
         $mainSlider.slick(options);
     })();
